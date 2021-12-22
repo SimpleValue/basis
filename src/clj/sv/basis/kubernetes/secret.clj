19ce4dda39b22f3bebac992f677690039721290a
Max Weber <weber.maximilian@gmail.com>
2021-12-22 18:23:07 +0100

    Added tools to manage Kubernetes secrets.

---
+++ src/clj/sv/basis/kubernetes/secret.clj
@@ -0,0 +1,39 @@
+(ns sv.basis.kubernetes.secret
+  (:require [babashka.process :as process]
+            [cheshire.core :as json]))
+
+(defn get-secret
+  [{:keys [secret-name]}]
+  (-> (process/process
+        ["kubectl" "get" "secret" secret-name "-o" "json"]
+        {:out :string})
+      (process/check)
+      (:out)
+      (json/parse-string true)))
+
+(defn save
+  [secret]
+  (process/check
+    (process/process
+      ["kubectl" "apply" "-f" "-"]
+      {:in (json/generate-string secret)})))
+
+(defn edit
+  [params f]
+  (-> (get-secret params)
+      (f)
+      (save)))
+
+(defn base64-encode [to-encode]
+  (.encodeToString (java.util.Base64/getUrlEncoder)
+                   to-encode))
+
+(defn add
+  [{:keys [key value] :as params}]
+  (edit params
+        (fn [secret]
+          (assoc-in secret
+                    [:data
+                     key]
+                    (base64-encode (.getBytes value
+                                              "UTF-8"))))))
