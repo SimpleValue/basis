3acc5dd0afb7cce743507b7c8bbded5b0d12dd98
Max Weber <weber.maximilian@gmail.com>
2021-12-23 19:57:56 +0100

    Decoupled edit! functions.

--- src/clj/sv/basis/kubernetes/secret.clj
+++ src/clj/sv/basis/kubernetes/secret.clj
@@ -19,26 +19,59 @@
       {:in (json/generate-string secret)})))
 
 (defn edit!
-  [params f]
-  (-> (get-secret params)
-      (f)
-      (save!)))
+  [params f & args]
+  (let [secret (get-secret params)
+        new-secret (apply f secret args)]
+    (save! new-secret)))
 
 (defn base64-encode [to-encode]
   (.encodeToString (java.util.Base64/getUrlEncoder)
                    to-encode))
 
-(defn set-key-value!
-  [{:keys [key value] :as params}]
-  (edit! params
-         (fn [secret]
-           (assoc-in secret
-                     [:data
-                      key]
-                     (base64-encode (.getBytes value
-                                               "UTF-8"))))))
+(defn- base64-decode [to-decode]
+  (.decode (java.util.Base64/getDecoder)
+           to-decode))
+
+(defn base64-vals
+  [a-map]
+  (into {}
+        (map (fn [[key value]]
+               [key
+                (base64-encode
+                 (.getBytes value
+                            "UTF-8"))])
+             a-map)))
+
+(defn string-vals
+  [a-map]
+  (into {}
+        (map (fn [[key value]]
+               [key
+                (String. (base64-decode value)
+                         "UTF-8")])
+             a-map)))
+
+(defn merge-key-values
+  [secret key-values]
+  (update-in secret
+             [:data]
+             merge
+             (base64-vals key-values)))
+
+(defn set-key-value
+  [secret key value]
+  (merge-key-values secret
+                    {key value}))
+
+(defn get-key-values
+  [params]
+  (-> params
+      (get-secret)
+      (:data)
+      string-vals))
 
 (comment
   (get-secret {:secret-name "app-env"})
 
+  (get-key-values {:secret-name "app-env"})
   )
